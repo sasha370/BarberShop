9975a95d40ebeedb7618a9fb5742714a342db7ed
Alex <kng.sasha@mail.ru>
2020-05-24 09:33:21 +0300

    Initial code for lesson 25

--- app.rb
+++ app.rb
@@ -13,3 +13,34 @@ end
 get '/visit' do
 	erb :visit
 end
+
+get '/login' do
+  erb :login
+end
+
+post '/visit' do
+  @username = params[:username]
+  @phone = params[:phone]
+  @datetime = params[:datetime]
+  @master = params[:master]
+
+  file = File.open('./public/contacts.txt', 'a')
+  file.write " Имя: #{@username}, Телефон: #{@phone}, Время: #{@datetime}, Мастер: #{@master} \n"
+  file.close
+  erb "Спасибо, #{@username}, Ваша заявка принята"
+end
+
+post '/login' do
+  @login = params[:login]
+  @password = params[:password]
+  if @login == 'admin' && @password == '123'
+
+
+    @message =  'Успешно'
+    erb :about
+  else
+    erb :login
+    @message = 'Неправильный логин или пароль'
+
+  end
+end
